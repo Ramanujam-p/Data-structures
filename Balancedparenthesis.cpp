ae1d429ab5669b845371eb34cf0996c08b08f84b
Ramanujam-p <pramanujam68@gmail.com>
2025-09-16 11:51:29 +0530

    balanced parentheses using manual stack

--- Balancedparenthesis.cpp
+++ Balancedparenthesis.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
 #include<string>
-#include<stack>
 using namespace std;
+#define M 100
+class stack
+{
+    char a[M];
+    int t;
+    public:
+    stack(){ t=-1;}
+    void push(char x)
+    {
+        if(t<M-1) a[++t] = x;
+        else cout<<"stack overflow";
+    }
+    void pop()
+    {
+        if(t>=0) t--;
+        else cout<<"stack underflow";
+    }
+    char peek()
+    {
+        if(t>=0) return a[t];
+        return '\0';
+    }
+    bool empty()
+    { 
+        return t == -1;
+    }
+};
 bool isvalid(const string& a)
 {
-    stack<char> st;
+    stack st;
     for(char c : a)
     {
         if( c == '(' || c == '{' || c == '[') st.push(c);
         else if( c == ')' || c == '}' || c == ']')
         {
             if(st.empty()) return false;
-                char top = st.top();
+                char top = st.peek();
                 st.pop();
                 if((c == ')' && top!='(') || ( c == '}' && top!='{') || (c == ']' && top!='[')) 
                 return false;
